63f2894cb7ef99a2648ebea7a2311305c069e21a
kirubiisfyne <kirby.bilbao@wvsu.edu.ph>
2025-05-26 23:17:07 +0800

    Added: OpenGallerie Feed, Profile Portfolio, Upload Image

--- app.py
+++ app.py
@@ -1,34 +1,41 @@
-from flask import Flask, render_template, url_for, request, redirect, jsonify, get_flashed_messages
+from flask import Flask, render_template, request, session, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
 from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
+app.secret_key = '1234'
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), nullable=False)
     password = db.Column(db.String(200), nullable=False)
     email = db.Column(db.String(100), nullable=False)
-
-    def __repr__(self):
-        return '<User %r>' % self.username
-
+    
+    portfolio = db.relationship('Portfolio', back_populates='user', lazy=True, uselist=False)
 class Portfolio(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    content = db.Column(db.JSON, nullable=True)
+    content = db.Column(db.JSON, nullable=True) 
 
-    user = db.relationship('User', backref='portfolio', lazy=True, uselist=False)
+    user = db.relationship('User', back_populates='portfolio', lazy=True, uselist=False) 
 class Image(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    portfolio_id = db.Column(db.Integer, db.ForeignKey('portfolio.id'), nullable=False)
     image_filename = db.Column(db.Text, nullable=False)
-    date_uploaded = db.Column(db.DateTime, default=datetime)
+    image_description = db.Column(db.Text, nullable=True)
+    date_uploaded = db.Column(db.DateTime, default=datetime.utcnow)
 
+    portfolio_id = db.Column(db.Integer, db.ForeignKey('portfolio.id'), nullable=False)
     portfolio = db.relationship('Portfolio', backref='images', lazy=True)
-
 def to_dict(self):
         return {
             'username': self.username,
@@ -42,12 +49,12 @@ def login():
         password = request.form['password']
 
         user = User.query.filter_by(username=username).first() # Query the database for the user
-
+        
         if user: # Checks if the user exists
-            print(f"User ID: {user.id}")
-            return render_template('open_gallery.html', user=user)
+            session['user_id'] = user.id
+            return redirect(url_for('open_gallery', user=user))
         else:
-            return render_template('signup_redirect.html', error="User not found")
+            return redirect(url_for('signup_redirect', error="User not found"))
     else:
         return render_template('login.html')
 
@@ -67,27 +74,107 @@ def signup():
                 db.session.add(new_user)
                 db.session.commit()
 
-                return render_template('open_gallery.html', user=new_user)
+                new_portfolio = Portfolio(user_id=new_user.id, user=new_user)
+                db.session.add(new_portfolio)
+                db.session.commit() 
+
+                session['user_id'] = new_user.id
+
+                return redirect(url_for('open_gallery', user=new_user))
         except Exception as e:
             print(f"Error creating user: {e}")
             return render_template('signup.html', error="Error creating user")
     else:
         return render_template('signup.html')
 
-@app.route('/open_gallery', methods=['POST', 'GET'])
+@app.route('/open_gallery', methods=['GET'])
+def open_gallery():
+    images = Image.query.all()
+    user = User.query.filter_by(id=session.get('user_id')).first()
+    if not images:
+        print("No images found in the database")
+    return render_template('open_gallery.html', images=images, user=user)
+
+@app.route('/delete_account', methods=['POST', 'GET'])
 def delete_account():
     if request.method == 'POST':
-        user_id = request.form.get('user_id')
-        print(user_id)
-        user = User.query.get(user_id)
+        user_id = session.get('user_id')
+        user = User.query.filter_by(id=user_id).first()
         if user:
+            for image in user.portfolio.images:
+                db.session.delete(image)
+            db.session.delete(Portfolio.query.filter_by(user_id=user.id).first())
             db.session.delete(user)
             db.session.commit()
             return render_template('login.html', message="Account deleted successfully")
         else:
-            return render_template('open_gallery.html', error="User not found")
+            return redirect(url_for('open_gallery', error="User not found"))
     else:
-        return render_template('open_gallery.html')
+        return redirect(url_for('open_gallery'))
+
+
+@app.route('/upload_image', methods=['POST', 'GET'])
+def upload_image():
+    if 'image' not in request.files:
+        return render_template('upload_image.html')
+    
+    user_id = session.get('user_id')
+    user = User.query.filter_by(id=user_id).first()
+    if not user:
+        return "User not found"
+
+    if request.method == 'POST':
+        file = request.files.get('image')
+        title = request.form.get('filename')
+        description = request.form.get('description')
+        
+        if Image.query.filter_by(image_filename=title).first():
+            flash('Image with this filename already exists!', 'error')
+            return redirect(url_for('upload_image'))
+
+        if file.filename == '':
+            return "No selected file"
+        if file and allowed_file(file.filename):
+            portfolio = user.portfolio
+            portfolio_id = portfolio.id   
+
+            new_file = Image(portfolio_id=portfolio_id, portfolio=portfolio, image_filename=title, image_description=description)
+            db.session.add(new_file)
+            db.session.commit()
+
+            file.save(os.path.join('static/uploads', title))
+
+            return redirect(url_for('display_profile'))
+        else:
+            return flash('No file selected!', 'error')
+    
+@app.route('/display_profile', methods=['GET'])
+def display_profile():
+    user_id = session.get('user_id')
+    user = User.query.filter_by(id=user_id).first()
+
+    images = user.portfolio.images if user and user.portfolio else []
+    return render_template('profile.html', user=user, images=images)
+
+@app.route('/delete_image', methods=['POST', 'GET'])
+def delete_image():
+    if request.method == 'POST':
+        image_filename = request.form.get('image_filename')
+        image = Image.query.filter_by(image_filename=image_filename).first()
+        if image:
+            file_path = os.path.join('static/uploads', image_filename)
+
+            if os.path.exists(file_path):
+                os.remove(file_path)
+            else:
+                print("File does not exist, cannot delete")
+
+            db.session.delete(image)
+            db.session.commit()
+            flash('Image deleted successfully', 'success')
+        else:
+            flash('Image not found', 'error')
+    return redirect(url_for('display_profile'))
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000,debug=True)
